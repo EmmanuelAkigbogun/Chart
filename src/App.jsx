67dd94de11373e58fb5c93fd0e7922b9d87a6322
EmmanuelAkigbogun <105459628+EmmanuelAkigbogun@users.noreply.github.com>
2024-04-05 16:32:56 +0100

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,19 @@ import {
   createRoutesFromElements,
 } from "react-router-dom";
 import DashBoard from "./Components/DashBoard";
-import Main from "./Components/Main";
 import "./index.css";
 import Chart from "./Components/Chart";
+import ChartA from "./Components/ChartA";
+import Page from "./Components/Page";
 function App() {
   let router = createBrowserRouter(
     createRoutesFromElements(
       <Route>
-          <Route element={<Chart />} path="chart"></Route>
-          <Route element={<DashBoard />} path="">
-            <Route element={<Main />} path=""></Route>
-          </Route>
+        <Route element={<ChartA />} path="chartx"></Route>
+        <Route element={<Chart />} path="chart"></Route>
+        <Route element={<DashBoard />} path="">
+          <Route element={<Page />} path=""></Route>
+        </Route>
       </Route>
     )
   );
